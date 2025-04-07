a13bee0552a8a54c89c0fa516d1cfa560aa940de
Markov <140595764+MarkovChenITRI@users.noreply.github.com>
2025-04-07 20:13:29 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 cd firecrawl/apps/api
 cp .env.example .env
-docker compose up -d
+sudo docker compose up -d
 cd ../../..
 
 cd dify/docker
 cp .env.example .env
-docker compose up -d
+sudo docker compose up -d
 cd ../..
