ac0e86f18551d3c752719b4aa1dec4d23af4a1a5
Markov <140595764+MarkovChenITRI@users.noreply.github.com>
2025-04-09 01:05:04 +0800

    update

--- ScraperGraphTest.py
+++ ScraperGraphTest.py
@@ -3,8 +3,7 @@ from scrapegraphai.graphs import SmartScraperGraph
 # Define the configuration for the scraping pipeline
 graph_config = {
     "llm": {
-        "model": "ollama/llama3.2",
-        "model_tokens": 8192
+        "model": "ollama/phi4:latest",
     },
     "verbose": True,
     "headless": False,
